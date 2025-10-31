e5f62b9b7608f796934ed180fb78db71b60af160
Minerva <minerva.martinez@globant.com>
2025-10-31 09:17:27 +0100

    Format document

--- src/api.js
+++ src/api.js
@@ -1,32 +1,32 @@
 // Función para devolver los datos de un Pokemon
 export async function fetchPokemon(id) {
- try{
+  try {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-  const data = await response.json();
+    const data = await response.json();
 
-  return {
-    name: data.name,
-    imageFront: data.sprites.front_default, 
-    imageBack: data.sprites.back_default,
-    attack: data.stats.find(stat => stat.stat.name === 'attack').base_stat,
-    type: data.types[0].type.name            
-  };
- } catch (error){
-    console.error ('Error fetching Pokémon: ', error);
+    return {
+      name: data.name,
+      imageFront: data.sprites.front_default,
+      imageBack: data.sprites.back_default,
+      attack: data.stats.find((stat) => stat.stat.name === "attack").base_stat,
+      type: data.types[0].type.name,
+    };
+  } catch (error) {
+    console.error("Error fetching Pokémon: ", error);
     return null;
- }
+  }
 }
 
 // Función para devolver los Pokemon de un tipo
 export async function fetchPokemonByType(type) {
   try {
     const response = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
-    if (!response.ok) throw new Error('Type not found');
+    if (!response.ok) throw new Error("Type not found");
     const data = await response.json();
 
-  return data.pokemon.map(p => p.pokemon); 
- } catch (error) {
-    console.error ('Error fetching Pokémon by type: ', error);
+    return data.pokemon.map((p) => p.pokemon);
+  } catch (error) {
+    console.error("Error fetching Pokémon by type: ", error);
     return [];
- }
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,30 +1,30 @@
-import './style.css'
-import { fetchPokemon, fetchPokemonByType } from './api.js';
+import "./style.css";
+import { fetchPokemon, fetchPokemonByType } from "./api.js";
 
-const startBtn = document.getElementById('startBtn');
-const battleBtn = document.getElementById('battleBtn');
-const pokemonContainer = document.getElementById('pokemonContainer');
-const resultContainer = document.getElementById('resultContainer');
-const typeContainer = document.getElementById('typeContainer');
+const startBtn = document.getElementById("startBtn");
+const battleBtn = document.getElementById("battleBtn");
+const pokemonContainer = document.getElementById("pokemonContainer");
+const resultContainer = document.getElementById("resultContainer");
+const typeContainer = document.getElementById("typeContainer");
 
 let pokemon1 = null;
 let pokemon2 = null;
-battleBtn.classList.add('hidden');
+battleBtn.classList.add("hidden");
 
-function randomId(){
-    return Math.floor(Math.random() * 1025) +1;
+function randomId() {
+  return Math.floor(Math.random() * 1025) + 1;
 }
 //Carga de los pokemons
-async function loadNewPokemons (){
-  try{
-    resultContainer.innerHTML = '';
-    typeContainer.innerHTML = '';
-    battleBtn.classList.add('hidden');
+async function loadNewPokemons() {
+  try {
+    resultContainer.innerHTML = "";
+    typeContainer.innerHTML = "";
+    battleBtn.classList.add("hidden");
 
     const id1 = randomId();
     const id2 = randomId();
 
-    const [p1,p2] = await Promise.all([fetchPokemon(id1), fetchPokemon(id2)]);
+    const [p1, p2] = await Promise.all([fetchPokemon(id1), fetchPokemon(id2)]);
     pokemon1 = p1;
     pokemon2 = p2;
 
@@ -45,93 +45,102 @@ async function loadNewPokemons (){
       <p>Type: ${p2.type}</p>
     </div>
   `;
-  
-  battleBtn.textContent = 'Battle';
-  battleBtn.disabled = false;
-  battleBtn.classList.remove('hidden');
 
-  pokemonContainer.querySelectorAll('img').forEach (img => {
-   const front = img.dataset.front;
-   const back = img.dataset.back;
-   
-    img.addEventListener('mouseenter',() => {
-    if(back) img.src = back;
-    });
-    img.addEventListener('mouseleave',() => {
-    img.src = front;
-    });
- });
+    battleBtn.textContent = "Battle";
+    battleBtn.disabled = false;
+    battleBtn.classList.remove("hidden");
 
-} catch (error){
-    console.error ('Error loading Pokemons', error);
-    pokemonContainer.innerHTML = '<p>Something went wrong while loading Pokemons</p>';
+    pokemonContainer.querySelectorAll("img").forEach((img) => {
+      const front = img.dataset.front;
+      const back = img.dataset.back;
+
+      img.addEventListener("mouseenter", () => {
+        if (back) img.src = back;
+      });
+      img.addEventListener("mouseleave", () => {
+        img.src = front;
+      });
+    });
+  } catch (error) {
+    console.error("Error loading Pokemons", error);
+    pokemonContainer.innerHTML =
+      "<p>Something went wrong while loading Pokemons</p>";
     battleBtn.disabled = true;
-}
+  }
 }
 //Función para la batalla
 async function startBattle() {
-    battleBtn.disabled=true;
+  battleBtn.disabled = true;
 
-    const messagesBattle = ['Fighting.','Fighting..','Fighting...'];
-    for (let i = 0; i< messagesBattle.length; i++) {
-        battleBtn.textContent = messagesBattle[i];
-        await new Promise (res => setTimeout(res, 1000));
-    }
+  const messagesBattle = ["Fighting.", "Fighting..", "Fighting..."];
+  for (let i = 0; i < messagesBattle.length; i++) {
+    battleBtn.textContent = messagesBattle[i];
+    await new Promise((res) => setTimeout(res, 1000));
+  }
 
-    let winner;
-    if (pokemon1.attack > pokemon2.attack) winner = pokemon1;
-    else if (pokemon2.attack > pokemon1.attack) winner = pokemon2;
-    else {
-        resultContainer.innerHTML = '<h2>Its a tie!</h2>';
-        battleBtn.textContent = 'Battle';
-        battleBtn.disabled = false;
-        return;
-    }
-    
-    battleBtn.textContent = 'Battle';
+  let winner;
+  if (pokemon1.attack > pokemon2.attack) winner = pokemon1;
+  else if (pokemon2.attack > pokemon1.attack) winner = pokemon2;
+  else {
+    resultContainer.innerHTML = "<h2>Its a tie!</h2>";
+    battleBtn.textContent = "Battle";
     battleBtn.disabled = false;
+    return;
+  }
 
-    resultContainer.innerHTML = `<h2>The Winner is:<br>${winner.name.toUpperCase()} (${winner.attack} HP)</h2>`;
+  battleBtn.textContent = "Battle";
+  battleBtn.disabled = false;
 
-//Para añadir animación con CSS al Pokemon ganador
-const pokemonDivs = pokemonContainer.querySelectorAll('.pokemon');
+  resultContainer.innerHTML = `<h2>The Winner is:<br>${winner.name.toUpperCase()} (${
+    winner.attack
+  } HP)</h2>`;
 
-pokemonDivs.forEach(div => {
-    if (div.querySelector('h3').textContent === winner.name){
-        div.classList.add('winner');
+  //Añadir animación con CSS al Pokemon ganador
+  const pokemonDivs = pokemonContainer.querySelectorAll(".pokemon");
+
+  pokemonDivs.forEach((div) => {
+    if (div.querySelector("h3").textContent === winner.name) {
+      div.classList.add("winner");
     } else {
-        div.classList.remove('winner');
+      div.classList.remove("winner");
     }
-});
+  });
 
-// Tipo de Ganador
-    typeContainer.innerHTML=`<p>Type: ${winner.type}</p>`;    
+  // Tipo de Ganador
+  typeContainer.innerHTML = `<p>Type: ${winner.type}</p>`;
 
-    const typePokemons = await fetchPokemonByType(winner.type);
-    const others = typePokemons.filter(p => p.name !== winner.name).sort(() => 0.5 - Math.random()).slice(0, 3); 
-    
-    const otherDetails = await Promise.all(
-        others.map(p => {
-        const id = p.url.split('/').filter(Boolean).pop(); 
-        return fetchPokemon(id);
-  })
-);
-if (otherDetails.length > 0) {
-  const html = otherDetails.map(p => `
+  const typePokemons = await fetchPokemonByType(winner.type);
+  const others = typePokemons
+    .filter((p) => p.name !== winner.name)
+    .sort(() => 0.5 - Math.random())
+    .slice(0, 3); 
+
+  const otherDetails = await Promise.all(
+    others.map((p) => {
+      const id = p.url.split("/").filter(Boolean).pop();
+      return fetchPokemon(id);
+    })
+  );
+  if (otherDetails.length > 0) {
+    const html = otherDetails
+      .map(
+        (p) => `
     <div class="pokemon">
       <h4>${p.name}</h4>
       <img src="${p.imageFront}" alt="${p.name}">
       <p>HP: ${p.attack}</p>
       <p>Type: ${p.type}</p>
     </div>
-  `).join('');
+  `
+      )
+      .join("");
 
-  typeContainer.innerHTML += `
+    typeContainer.innerHTML += `
     <h3>Other ${winner.type}-type Pokémon:</h3>
     <div class="sameType">${html}</div>
   `;
- }
+  }
 }
 //Eventos que llaman a las funciones
-startBtn.addEventListener('click', loadNewPokemons);
-battleBtn.addEventListener('click', startBattle);
+startBtn.addEventListener("click", loadNewPokemons);
+battleBtn.addEventListener("click", startBattle);
